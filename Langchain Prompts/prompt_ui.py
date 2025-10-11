2887385c903ac1833efb8d4a311d591e71bc82f4
Deepak Kumar Mohanty <kd.codegeek@gmail.com>
2025-10-11 12:52:14 +0530

    lanchain models added completely and 1 prgram ofg prompt added

---
+++ Langchain Prompts/prompt_ui.py
@@ -0,0 +1,15 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import streamlit as st
+
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+load_dotenv()
+
+st.header('Research Paper Summarizer')
+
+user_input = st.text_area("Enter the research paper text here:")
+
+if st.button("summarize"):
+    result = model.invoke(user_input)
+    st.write(result.content)
